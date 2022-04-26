826c04531f79d621b240bafb372c2fb838f717ae
0xF16 <0xf16@protonmail.com>
2022-04-26 16:39:14 +0200

    fully working contract

--- contracts/BetVault.sol
+++ contracts/BetVault.sol
@@ -11,22 +11,26 @@ contract BetVault {
         uint256 bidPrice;
         uint256 bidCollateral;
     }
+    enum Status {WIP, AWAITING_WITHDRAWAL, CLOSED}
+    uint256 MAX_UINT = 2**256 - 1;
 
-    address public assetAddr;
     AggregatorV3Interface internal priceFeed;
     uint256 public biddingTimeEnd;
     uint256 public endTime;
 
     mapping(address => Bet) public bets;
     address[] public bidders;
+    Status private status;
+    uint256 public priceFromOracle;
+    address[] public winners;
 
-    constructor(address _assetAddr, address _priceFeed, uint256 _biddingTimeEnd, uint256 _endTime) {
+    constructor(address _priceFeed, uint256 _biddingTimeEnd, uint256 _endTime) {
         require(_biddingTimeEnd <= _endTime, "Bidding time must be before whole bid ends");
         require(_biddingTimeEnd > block.timestamp, "Bidding must end after now");
-        assetAddr = _assetAddr;
         biddingTimeEnd = _biddingTimeEnd;
         endTime = _endTime;
         priceFeed = AggregatorV3Interface(_priceFeed);
+        status = Status.WIP;
     }
 
     function placeBid(uint256 _bidPrice) external payable {
@@ -34,5 +38,81 @@ contract BetVault {
         require(block.timestamp < biddingTimeEnd, "Too late to place a bid");
         bets[msg.sender] = Bet(true, _bidPrice, msg.value);
         bidders.push(msg.sender);
+    }
+
+    function assesPrice() external {
+        require(status == Status.WIP, "Price already fetched");
+        require(block.timestamp > endTime, "It's not yet time to check prices");
+        (uint256 _priceFromOracle, uint8 decimals) = getLatestPrice();
+        priceFromOracle = _priceFromOracle/uint256(10**decimals);
+        status = Status.AWAITING_WITHDRAWAL;
+    }
+
+    function checkWhoWon() external {
+        require(status == Status.AWAITING_WITHDRAWAL, "Assess the price of an asset first");
+        uint256 bestDiff = MAX_UINT;
+        uint256 bestBid;
+        uint256 n_best_bids;
+
+        for(uint256 i; i < bidders.length; i++) {
+            uint256 _diff;
+            uint256 bid = bets[bidders[i]].bidPrice;
+            if (bid > priceFromOracle) {
+                _diff = bid - priceFromOracle;
+            } else if (bid <= priceFromOracle) {
+                _diff = priceFromOracle - bid;
+            }
+
+            if(_diff < bestDiff) {
+                bestBid = bid;
+                bestDiff = _diff;
+                n_best_bids = 1;
+            } else if(_diff == bestDiff) {
+                n_best_bids++;
+            }
+        }
+
+        address[] memory bestBidders = new address[](n_best_bids);
+        uint256 j;
+        for(uint256 i; i < bidders.length; i++) {
+            uint256 bid = bets[bidders[i]].bidPrice;
+            if(bid == bestBid) {
+                bestBidders[j] = bidders[i];
+                j++;
+            }
+        }
+        winners = bestBidders;
+    }
+
+    function withdrawal() external {
+        uint256 reward = address(this).balance/winners.length;
+        for(uint256 i; i < winners.length; i++) {
+            (bool success, ) = winners[i].call{value: reward}("");
+            require(success, "Failed to send reward to one of the addresses");
+        }
+        status = Status.CLOSED;
+    }
+
+    function close() external {
+        if(status == Status.WIP) this.assesPrice();
+        this.checkWhoWon();
+        this.withdrawal();
+    }
+
+    function winnersCount() external view returns (uint256) {
+        return winners.length;
+    }
+
+    function getLatestPrice() internal view returns (uint256, uint8) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        uint8 decimals = priceFeed.decimals();
+        if(price < 0) return (uint(-price), decimals);
+        return (uint(price), decimals);
     }
 }
--- test/bet-vault.js
+++ test/bet-vault.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { parseEther } = require("hardhat").ethers.utils;
 
-const priceOracle = "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612"; //Arbitrum Mainnet ETH/USD price https://docs.chain.link/docs/arbitrum-price-feeds/
+const priceOracle = "0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8"; //Arbitrum Rinkeby ETH/USD price https://docs.chain.link/docs/arbitrum-price-feeds/
 
 describe("BetVault contract deployment", () => {
   let BetVault;
@@ -13,18 +14,18 @@ describe("BetVault contract deployment", () => {
   });
   it("Cannot deploy a vault with bidding that ends before whole session end", async function () {
 
-    await expect(BetVault.deploy(deployer.address, priceOracle, 1650884797, 1650884796)).to.be.revertedWith("Bidding time must be before whole bid ends");
+    await expect(BetVault.deploy(priceOracle, 1650884797, 1650884796)).to.be.revertedWith("Bidding time must be before whole bid ends");
   });
 
   it("Cannot deploy a vault with time that already passed", async function () {
     let lastBlockTime = await ethers.provider.getBlock("latest");
     lastBlockTime = lastBlockTime.timestamp;
 
-    await expect(BetVault.deploy(deployer.address, priceOracle, lastBlockTime-10, lastBlockTime+10)).to.be.revertedWith("Bidding must end after now");
+    await expect(BetVault.deploy(priceOracle, lastBlockTime-10, lastBlockTime+10)).to.be.revertedWith("Bidding must end after now");
   });
 });
 
-describe("BetVault placing bets", () => {
+describe("BetVault placing bids", () => {
   let BetVault;
   let betVault;
   let deployer, address1, address2;
@@ -34,7 +35,7 @@ describe("BetVault placing bets", () => {
     BetVault = await ethers.getContractFactory("BetVault");
     let lastBlockTime = await ethers.provider.getBlock("latest");
     lastBlockTime = lastBlockTime.timestamp;
-    betVault = await BetVault.deploy(deployer.address, priceOracle, lastBlockTime+10, lastBlockTime+20);
+    betVault = await BetVault.deploy(priceOracle, lastBlockTime+10, lastBlockTime+20);
   });
   it("Can place bids", async function () {
     await betVault.connect(address1).placeBid(3000, {value: ethers.utils.parseEther('1')});
@@ -52,3 +53,93 @@ describe("BetVault placing bets", () => {
     expect(await ethers.provider.getBalance(betVault.address)).to.equal(ethers.utils.parseUnits('1.5', 'ether'));
   });
 });
+
+describe("BetVault closing bets", () => {
+  let BetVault;
+  let betVault;
+  let deployer, address1, address2;
+
+  beforeEach(async () => {
+    [deployer, address1, address2, address3] = await ethers.getSigners();
+    BetVault = await ethers.getContractFactory("BetVault");
+    let lastBlockTime = await ethers.provider.getBlock("latest");
+    lastBlockTime = lastBlockTime.timestamp;
+    betVault = await BetVault.deploy(priceOracle, lastBlockTime+10, lastBlockTime+11);
+  });
+  it("Cannot assess price before bidding period is finished", async function () {
+    await betVault.connect(address1).placeBid(3000, {value: ethers.utils.parseEther('1')});
+    await betVault.connect(address2).placeBid(3500, {value: ethers.utils.parseEther('0.5')});
+
+    await expect(betVault.assesPrice()).to.be.revertedWith("It's not yet time to check prices");
+  });
+  it("Assess price", async function () {
+    await betVault.connect(address1).placeBid(3000, {value: ethers.utils.parseEther('1')});
+    await betVault.connect(address2).placeBid(3500, {value: ethers.utils.parseEther('0.5')});
+    await mineNBlocks(11);
+    await betVault.assesPrice();
+    const price = await betVault.priceFromOracle(); //299996245016
+
+    expect(price.toNumber()).to.equal(2999);
+  });
+  it("Select a winner (1 winner)", async function () {
+    await betVault.connect(address1).placeBid(3000, {value: ethers.utils.parseEther('1')});
+    await betVault.connect(address2).placeBid(3500, {value: ethers.utils.parseEther('0.5')});
+    await betVault.connect(address3).placeBid(4000, {value: ethers.utils.parseEther('0.5')});
+    await mineNBlocks(11);
+    await betVault.assesPrice();
+    await betVault.checkWhoWon();
+    const winnersCount  = await betVault.winnersCount();
+
+    expect(winnersCount).to.equal(1);
+  });
+  it("Select a winner (2 got the same bid)", async function () {
+    await betVault.connect(address1).placeBid(3000, {value: ethers.utils.parseEther('1')});
+    await betVault.connect(address2).placeBid(3500, {value: ethers.utils.parseEther('0.5')});
+    await betVault.connect(address3).placeBid(3000, {value: ethers.utils.parseEther('0.5')});
+    await mineNBlocks(11);
+    await betVault.assesPrice();
+    await betVault.checkWhoWon();
+    const winnersCount  = await betVault.winnersCount();
+
+    expect(winnersCount).to.equal(2);
+  });
+  it("Withdraw reward (1 user)", async function () {
+    await betVault.connect(address1).placeBid(2900, {value: ethers.utils.parseEther('1')});
+    await betVault.connect(address2).placeBid(3500, {value: ethers.utils.parseEther('0.5')});
+    await mineNBlocks(11);
+    const winnerBalanceBefore = await ethers.provider.getBalance(address1.address);
+    const looserBalanceBefore = await ethers.provider.getBalance(address2.address);
+    await betVault.close();
+    const winnerBalanceAfter = await ethers.provider.getBalance(address1.address);
+    const looserBalanceAfter = await ethers.provider.getBalance(address2.address);
+    const contractBalanceAfter = await ethers.provider.getBalance(betVault.address);
+
+    const resultMatrix = [winnerBalanceAfter.gt(winnerBalanceBefore), looserBalanceBefore.eq(looserBalanceAfter), contractBalanceAfter.isZero()];
+
+    expect([true, true, true]).to.eql(resultMatrix);
+  });
+  it("Withdraw reward (2 users)", async function () {
+    await betVault.connect(address1).placeBid(2900, {value: ethers.utils.parseEther('1')});
+    await betVault.connect(address2).placeBid(3500, {value: ethers.utils.parseEther('0.5')});
+    await betVault.connect(address3).placeBid(3000, {value: ethers.utils.parseEther('0.5')});
+    await mineNBlocks(11);
+    const winnerBalanceBefore = await ethers.provider.getBalance(address3.address);
+    const looser1BalanceBefore = await ethers.provider.getBalance(address1.address);
+    const looser2BalanceBefore = await ethers.provider.getBalance(address2.address);
+    await betVault.close();
+    const winnerBalanceAfter = await ethers.provider.getBalance(address3.address);
+    const looser1BalanceAfter = await ethers.provider.getBalance(address1.address);
+    const looser2BalanceAfter = await ethers.provider.getBalance(address2.address);
+    const contractBalanceAfter = await ethers.provider.getBalance(betVault.address);
+
+    const resultMatrix = [winnerBalanceAfter.gt(winnerBalanceBefore), looser1BalanceBefore.eq(looser1BalanceAfter), looser2BalanceBefore.eq(looser2BalanceAfter), contractBalanceAfter.isZero()];
+
+    expect([true, true, true, true]).to.eql(resultMatrix);
+  });
+});
+
+async function mineNBlocks(n) {
+  for (let index = 0; index < n; index++) {
+    await ethers.provider.send('evm_mine');
+  }
+}
