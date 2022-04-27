a1b4ae28257a62bf1443aa0e1f78ee27a7a6f9f2
0xF16 <0xf16@protonmail.com>
2022-04-27 12:51:34 +0200

    prepare deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy
+  const BetVault = await hre.ethers.getContractFactory("BetVault");
+  const betVault = await BetVault.deploy('0x7f8847242a530E809E17bF2DA5D2f9d2c4A43261', 1651011900, 1651011900);
+
+  await betVault.deployed();
+
+  console.log("BetVault deployed to:", betVault.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
